7e253afc462a776d3678f1d923c9d6cbaaa845d1
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2025-02-11 08:52:09 +0100

    Initial Commit

---
+++ src/main/java/at/ac/htlstp/et/s24k4b/sb1/S24k4bspringbootApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.s24k4b.sb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24k4bspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S24k4bspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/htlstp/et/s24k4b/sb1/S24k4bspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.s24k4b.sb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S24k4bspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
